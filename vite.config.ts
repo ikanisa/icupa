4f9d5cd9da4f949a22fd8e38913bffbae5519588
Your Name <you@example.com>
2025-10-24 22:26:27 +0200

    build(vite): remove lovable-tagger plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
@@ -28,7 +27,7 @@ export default defineConfig(({ mode }) => ({
         type: "module",
       },
     }),
-    mode === "development" && componentTagger(),
+    
   ].filter(Boolean),
   resolve: {
     alias: {
